36dbdcb57f0d6b7e84e200950c484c3dd36aca42
Stefan Waldmann <stefan.waldmann@doubleslash.de>
2019-03-04 12:49:51 +0100

    - Added simple producer example (with KafkaTemplate, without Avro) - Consumer now uses the topic 'kafka-demo' the simple producer writes to - Adjusted README.

--- consumer/src/main/java/de/doubleslash/demo/kafka/KafkaSpringConsumerDemoApp.java
+++ consumer/src/main/java/de/doubleslash/demo/kafka/KafkaSpringConsumerDemoApp.java
@@ -10,7 +10,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 
 /**
- * Kafka Consumer listening to the logging-counts topic.
+ * Kafka Consumer listening to the "kafka-demo" topic.
  * <p>
  * This class uses the kafka integration of Spring Boot.
  */
@@ -33,9 +33,9 @@ class LogMessageCountConsumer {
 
     Logger LOG = LoggerFactory.getLogger(LogMessageCountConsumer.class);
 
-    @KafkaListener(topics = "logging-counts")
-    public void consumeMessage(ConsumerRecord<String, Long> record) {
-        LOG.info("{} => {}", record.key(), record.value());
+    @KafkaListener(topics = "kafka-demo")
+    public void consumeMessage(ConsumerRecord<String, String> record) {
+        LOG.info("{}", record.value());
     }
 
 }
---
+++ producer-demo/src/main/java/de/doubleslash/demo/kafka/producer/KafkaTemplateProducerDemoApp.java
@@ -0,0 +1,73 @@
+
+package de.doubleslash.demo.kafka.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import com.thedeanda.lorem.Lorem;
+import com.thedeanda.lorem.LoremIpsum;
+
+@SpringBootApplication
+public class KafkaTemplateProducerDemoApp {
+
+    @Bean
+    CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+        return args -> new KafkaProducerDemo(kafkaTemplate).publishRandomMessages();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTemplateProducerDemoApp.class);
+    }
+}
+
+class KafkaProducerDemo {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaProducerDemo.class);
+
+    private static final Lorem LOREM = LoremIpsum.getInstance();
+
+    private boolean running = false;
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducerDemo(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    void publishRandomMessages() {
+        this.running = true;
+
+        while(running) {
+            String message = randomMessage();
+
+            publish(message);
+
+            LOG.info("Message '{}' was published to Kafka.", message);
+            sleep();
+        }
+    }
+
+    private String randomMessage() {
+        String msg = LOREM.getWords(5, 10) + ".";
+        msg = msg.substring(0, 1).toUpperCase() + msg.substring(1);
+        return msg;
+    }
+
+    void publish(String message) {
+        kafkaTemplate.send("kafka-demo", null, message);
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
